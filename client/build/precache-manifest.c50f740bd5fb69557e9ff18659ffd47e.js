d90cc7beabd9f29d551c66f147e4b6b630793a49
brandon <contactbrandonye@gmail.com>
2020-04-24 01:51:35 -0400

    deploy to heroku

---
+++ client/build/precache-manifest.c50f740bd5fb69557e9ff18659ffd47e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0134688c2fa0d2fdf2b738d8681639cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d1220b43188c5ea540c",
+    "url": "/static/css/main.786326c6.chunk.css"
+  },
+  {
+    "revision": "9252c99aa31f23b95dc3",
+    "url": "/static/js/2.fe9ac58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe9ac58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1220b43188c5ea540c",
+    "url": "/static/js/main.8ff77563.chunk.js"
+  },
+  {
+    "revision": "a5581670c6fa053af376",
+    "url": "/static/js/runtime-main.71369147.js"
+  }
+]);
